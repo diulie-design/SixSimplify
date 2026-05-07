9104578049c606613c298ab2d0484f2f469b6ede
VRO-lab <borndiulie@gmail.com>
2026-05-07 17:09:12 -0300

    Create 2_Principais_Obstaculos.py

---
+++ pages/2_Principais_Obstaculos.py
@@ -0,0 +1,12 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="Principais Obstáculos",
+    layout="wide"
+)
+
+st.title("Principais Obstáculos")
+
+st.info(
+    "Essa aba está reservada para a próxima etapa."
+)
